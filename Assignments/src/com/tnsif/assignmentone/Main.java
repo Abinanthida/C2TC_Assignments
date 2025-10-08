896119b415d25f5ad8b8e0d27d9dcbed8e1438f4
Abinanthida <abigowrish2004@gmail.com>
2025-10-08 10:22:18 +0530

    Assignment2

---
+++ Assignments/src/com/tnsif/assignmentone/Main.java
@@ -0,0 +1,16 @@
+package com.tnsif.assignmentone;
+
+public class Main {
+	public static void main(String[] args) {
+		Student s = new Student();
+
+        // Create Commission object
+        Commission c = new Commission();
+
+        // Accept details and calculate commission
+        c.acceptDetails();
+        c.calculateCommission();
+
+
+}
+}
